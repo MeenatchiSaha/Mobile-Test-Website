cae2d9b36dfd136a54a3718efcb72d5dbfb952a8
Meena <meenatchimcse@gmail.com>
2022-02-22 15:08:19 +0530

    add filter

--- script.js
+++ script.js
@@ -112,3 +112,24 @@
 $(document).ready(function(){
   $('.multi_select').selectpicker();
 })
+
+
+function change() {
+  let results = Array.from(document.querySelectorAll('.result > div'));
+  // Hide all results
+  results.forEach(function(result) {
+    result.style.display = 'none';
+  });
+  // Filter results to only those that meet ALL requirements:
+  Array.from(document.querySelectorAll('.filter input[rel]:checked'), function(input) {
+    const attrib = input.getAttribute('rel');
+    results = results.filter(function(result) {
+      return result.classList.contains(attrib);
+    });
+  });
+  // Show those filtered results:
+  results.forEach(function(result) {
+    result.style.display = 'block';
+  });
+}
+change();
